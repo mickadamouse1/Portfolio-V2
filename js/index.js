0e34caa03f373d57c1a7041c7f61cc0250d430f8
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2020-02-05 23:14:39 +0000

    Optimised code, fixed skills dropdown animation bugs

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-  const btnAbout = document.getElementById("btnAbout");
-  const btnSkills = document.getElementById("btnSkills");
-  const btnProjects = document.getElementById("btnProjects");
-  const btnContact = document.getElementById("btnContact");
+  const btnAbout = document.getElementById('btnAbout');
+  const btnSkills = document.getElementById('btnSkills');
+  const btnProjects = document.getElementById('btnProjects');
+  const btnContact = document.getElementById('btnContact');
 
-  const navUnderline = document.getElementById("underline");
+  const navUnderline = document.getElementById('underline');
 
   const classSkillsExpanded = document.getElementsByClassName('skillsExpanded');
   const learningSkillsCard = document.getElementsByClassName('learningSkillsCard');
@@ -16,14 +16,14 @@ window.onload = () => {
 
   // NAVIGATION BAR 
 
-  navUnderline.style.display = "block";
+  navUnderline.style.display = 'block';
 
   let activeButton = btnAbout;
 
   /////////////////////////////////////////////////////////////////////////////////////////
 
   window.addEventListener('resize', () => {
-    navUnderline.style.transition = "none";
+    navUnderline.style.transition = 'none';
     if (activeButton !== btnContact) applyActiveStyle(activeButton);
 
     // resets the colour of the "Contact Me" button when the window is resized
@@ -43,7 +43,7 @@ window.onload = () => {
     let x = arrNavItems[i].id;
 
     arrNavItems[i].addEventListener('click', () => {
-      navUnderline.style.transition = ".5s ease";
+      navUnderline.style.transition = '.5s ease';
       activeButton = arrNavItems[i];
       applyActiveStyle(arrNavItems[i]);
 
@@ -62,10 +62,10 @@ window.onload = () => {
   /////////////////////////////////////////////////////////////////////////////////////////
 
   navHamburger.addEventListener('click', () => {
-    if (navItems.classList.contains("navExpanded")) {
-      navItems.classList.remove("navExpanded");
+    if (navItems.classList.contains('navExpanded')) {
+      navItems.classList.remove('navExpanded');
     } else {
-      navItems.classList.add("navExpanded");
+      navItems.classList.add('navExpanded');
     }
   });
 
@@ -74,28 +74,28 @@ window.onload = () => {
   applyActiveStyle = element => {
     const arr = [btnAbout, btnSkills, btnProjects];
     for (var i = 0; i < arr.length; i++) {
-      arr[i].classList.remove("active");
+      arr[i].classList.remove('active');
     }
-    element.classList.toggle("active");
+    element.classList.toggle('active');
 
     if (element !== btnContact && navUnderline.classList.contains('fadeOut')) navUnderline.classList.remove('fadeOut');
 
     if (element === btnContact) {
-      navUnderline.style.transition = ".1s ease";
+      navUnderline.style.transition = '.1s ease';
       navUnderline.classList.add('fadeOut');
 
       // This only applies when the nav isnt a dropdown
       if (window.innerWidth > 786) {
-        btnContact.style.transition = ".25s ease";
-        btnContact.style.background = "#212529";
+        btnContact.style.transition = '.25s ease';
+        btnContact.style.background = '#212529';
 
         // after .25 seconds, reset the color
         setTimeout(() => {
-          btnContact.style.background = "#20c997";
+          btnContact.style.background = '#20c997';
 
           // another timeout stops the animation from ending too early
           setTimeout(() => {
-            btnContact.style.transition = "none";
+            btnContact.style.transition = 'none';
           }, 250)
         }, 250);
       }
@@ -131,7 +131,8 @@ window.onload = () => {
   var classSkillCard = document.getElementsByClassName('learningSkillsCard');
 
   btnSkillsDropDownArrow.addEventListener('click', () => {
-    groupSkillsLearning.classList.toggle("skillsExpanded");
+    groupSkillsLearning.style.transition = '.5s ease';
+    groupSkillsLearning.classList.toggle('skillsExpanded');
     if (classSkillCard[0].classList.contains('fadeOut')) {
       setTimeout(function (){
         for (let i = 0; i < classSkillCard.length; i++) {
@@ -169,5 +170,5 @@ window.onload = () => {
 
   setTimeout(() => {
     window.scrollTo(0, 0);
-  },0)
+  },0);
 }
